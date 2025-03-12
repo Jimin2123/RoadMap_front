7e3a95df1305fbaeb3a596924fde31a42f04b4df
Jupiter <kjimin2123@gmail.com>
2025-03-13 00:53:36 +0900

    style(config): Add Prettier and ESLint configuration files
    - Prettier 설정 파일(.prettierrc) 추가
    - ESLint 설정 파일(eslint.config.js) 수정하여 Prettier 통합
    - 코드 스타일 일관성을 위해 코드 포맷팅 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] }, // dist, node_modules는 검사 대상에서 제외
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended, // JavaScript 기본 권장 규칙
+      ...tseslint.configs.recommended, // TypeScript 권장 규칙
+      prettierConfig, // prettier 설정 추가
+    ],
+    files: ['**/*.{ts,tsx}'], // TypeScript 파일에만 적용
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // 2020 ECMAScript 문법 지원
+      globals: globals.browser, // 브라우저 환경에서 사용 가능한 전역 변수 추가 (window, document 등)
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react, /// React 관련 ESLint 규칙 적용
+      'react-hooks': reactHooks, // React Hooks 관련 규칙 적용
+      'react-refresh': reactRefresh, // React Refresh 관련 규칙 적용
+      prettier, // prettier 플러그인 추가
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'prettier/prettier': 'error', // Prettier와 일치하지 않는 스타일이 있으면 ESLint 오류로 표시
+      // React Hooks 관련 규칙
+      ...reactHooks.configs.recommended.rules, // React Hooks 권장 규칙 적용
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }], // React Refresh 권장 규칙 적용
     },
-  },
-)
+  }
+);
