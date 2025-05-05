e5976b9ade7a62d2c1060ce483021fc900ae757a
SpeUjin <believeyu@naver.com>
2025-05-06 04:23:43 +0900

    Feat(Layout) : ServiceCard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 import EventBanner from './components/EventBanner/EventBanner';
 import LoginForm from './components/LoginForm/LoginForm';
+import ServiceCard from './components/ServiceCard/ServiceCard';
 
 function App() {
   return (
@@ -17,9 +18,32 @@ function App() {
         </div>
       </div>
 
-      <main className="content">
-        <h1>정책 목록</h1>
-        {/* 콘텐츠 */}
+      <main className="middle-section">
+        <ServiceCard
+          imageUrl="/img1.jpg"
+          serviceName="나의 이력서 관리"
+          serviceDescription="자신의 포트폴리오를 작성하거나 수정할 수 있습니다."
+        />
+        <ServiceCard
+          imageUrl="/img2.jpg"
+          serviceName="나의 취직 정보"
+          serviceDescription="현재 진행중인 채용정보를 확인할 수 있습니다."
+        />
+        <ServiceCard
+          imageUrl="/img3.jpg"
+          serviceName="나의 역량 진단"
+          serviceDescription="사용자가 원하는 분야의 직무 역량과 비교하여 사용자의 부족한 역량을 채울 수 있는 방법을 제공합니다."
+        />
+        <ServiceCard
+          imageUrl="/img4.jpg"
+          serviceName="나의 청년 지원정책"
+          serviceDescription="현재 진행중인 청년 지원정책 리스트를 볼 수 있습니다."
+        />
+        <ServiceCard
+          imageUrl="/img5.jpg"
+          serviceName="나의 관심기업 리스트"
+          serviceDescription="사용자가 등록해둔 관심 기업 리스트를 보여줍니다."
+        />
       </main>
 
       <Footer />
--- src/components/EventBanner/EventBanner.tsx
+++ src/components/EventBanner/EventBanner.tsx
@@ -1,13 +1,14 @@
 // components/EventBanner.tsx
 import React from 'react';
 import './EventBanner.css';
+import banner from '../../assets/banner.jpg';
 
 const EventBanner: React.FC = () => {
   return (
     <div className="event-banner">
-      <img src="/images/event-banner.jpg" alt="이벤트 배너" className="event-banner-image" />
+      <img src={banner} className="event-banner-image" />
       {/* 텍스트 오버레이가 필요하다면 여기에 추가 */}
-      <div className="event-banner-text">
+      <div className="event-banner-text" hidden>
         <h2>2025 봄맞이 취업 이벤트</h2>
         <p>지금 참여하고 다양한 혜택을 받아보세요!</p>
       </div>
