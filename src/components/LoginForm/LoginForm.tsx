bc3ef18ad0ed162e88b94f6e57a795a28260d5b9
Jupiter <kjimin2123@gmail.com>
2025-05-06 12:03:58 +0900

    feat(LoginForm): Update login form layout and styles
    - 로그인 폼의 제목을 span으로 변경하고 중앙 정렬을 적용했습니다.
    - 로그인 버튼을 포함한 폼 요소를 추가하여 구조를 개선했습니다.
    - SNS 로그인 버튼의 스타일을 수정하고 아이콘 크기를 조정했습니다.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -5,12 +5,15 @@ import './LoginForm.css';
 const LoginForm: React.FC = () => {
   return (
     <div className="login-form-container">
-      <h2 className="login-title">로그인</h2>
+      <span className="login-title">
+        길라<span className="logo-job">JOB</span>
+      </span>
 
-      <input type="text" placeholder="아이디" className="login-input" />
-      <input type="password" placeholder="비밀번호" className="login-input" />
-
-      <button className="login-button">로그인</button>
+      <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+        <input type="text" placeholder="아이디" className="login-input" />
+        <input type="password" placeholder="비밀번호" className="login-input" />
+        <button className="login-button">로그인</button>
+      </form>
 
       <div className="login-links">
         <a href="#">아이디 찾기</a>
@@ -20,17 +23,17 @@ const LoginForm: React.FC = () => {
         <a href="#">회원가입</a>
       </div>
 
-      <div className="login-divider">- 간편 로그인 -</div>
+      <div className="login-divider">간편 로그인</div>
 
       <div className="sns-login-buttons-horizontal">
+        <button className="sns-icon-button naver" aria-label="네이버 로그인">
+          <img src="/icons/naver.svg" alt="naver" />
+        </button>
         <button className="sns-icon-button kakao" aria-label="카카오 로그인">
           <img src="/icons/kakao.svg" alt="kakao" />
         </button>
         <button className="sns-icon-button google" aria-label="구글 로그인">
           <img src="/icons/google.svg" alt="google" />
-        </button>
-        <button className="sns-icon-button naver" aria-label="네이버 로그인">
-          <img src="/icons/naver.svg" alt="naver" />
         </button>
       </div>
     </div>
