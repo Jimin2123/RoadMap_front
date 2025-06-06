e148177a75d775ce73d4eaba50de1a1b08c6888f
Jupiter <kjimin2123@gmail.com>
2025-06-07 01:07:48 +0900

    테스트

---
+++ src/services/taskService.ts
@@ -0,0 +1,8 @@
+import { SaraminJobListResponse } from '../types/interfaces/response/SaraminJobListResponse';
+import axiosInstance from '../utils/axiosInstance';
+
+export const getJobsListService = async (page: number): Promise<SaraminJobListResponse> => {
+  const response = await axiosInstance.get<SaraminJobListResponse>(`/api/v1/jobs?page=${page}`);
+
+  return response.data;
+};
