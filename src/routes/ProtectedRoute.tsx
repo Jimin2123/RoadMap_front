9a2d431113ce4008b87ea3e1b2e1d9432323718c
Jupiter <kjimin2123@gmail.com>
2025-06-11 16:22:47 +0900

    feat(routes): Add ProtectedRoute for authentication checks
    - 인증되지 않은 사용자가 접근할 수 없는 페이지에 대해 ProtectedRoute를 추가했습니다. 사용자가 로그인하지 않은 경우 SweetAlert2를 사용하여 로그인 경고 메시지를 표시하고, 로그인 페이지로 리다이렉트합니다.
    - 설정 페이지와 이력서 페이지에 ProtectedRoute를 적용했습니다.

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,37 @@
+import { useAppSelector } from '../store/hooks';
+import { RootState } from '../types/store';
+import { useNavigate } from 'react-router-dom';
+import { JSX, useEffect } from 'react';
+import Swal from 'sweetalert2';
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated, status } = useAppSelector((state: RootState) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated && status.initialize === 'rejected') {
+      Swal.fire({
+        title: '로그인이 필요합니다',
+        text: '로그인 하시겠습니까?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: '로그인하러 가기',
+        cancelButtonText: '취소',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/login');
+        } else {
+          navigate('/');
+        }
+      });
+    }
+  }, [isAuthenticated, status.initialize, navigate]);
+
+  if (status.initialize === 'pending' || status.initialize === 'idle') {
+    return <div className="loading-container">로딩 중...</div>;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
