f8a6adb9dc266d0d9eac9e875ec5a0acb50300ad
izuna69 <94025204+izuna69@users.noreply.github.com>
2025-06-12 15:10:08 +0900

    feat(resume) : ResumeView로 이름 변경

--- src/components/Resume/ResumeView.tsx
+++ src/components/Resume/ResumeView.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Resume.css';
+import './ResumeView.css';
 
-const Resume: React.FC = () => {
+const ResumeView: React.FC = () => {
   const navigate = useNavigate();
 
   const [form] = useState({
@@ -161,4 +161,4 @@ const Resume: React.FC = () => {
   );
 };
 
-export default Resume;
+export default ResumeView;
