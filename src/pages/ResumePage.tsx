a370176e8a3fdc2272994056508842bea6b8e7e5
Jupiter <kjimin2123@gmail.com>
2025-06-16 17:32:38 +0900

    feat(job-card): Add URL prop to JobCard component
    - JobCard 컴포넌트에 URL prop을 추가하여 링크를 지원합니다.
    - 사용자가 회사 공고를 클릭하면 해당 URL로 이동할 수 있습니다.
    - JobPostingSection에서 JobCard에 URL을 전달하도록 수정했습니다.

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from '../layouts/Header';
 import Resume from '../components/Resume/Resume';
 import Footer from '../layouts/Footer';
+import { useAppSelector } from '../store/hooks';
+import { RootState } from '../types/store';
+import ResumeView from '../components/Resume/ResumeView';
 
 const ResumePage: React.FC = () => {
+  const { member, status } = useAppSelector((state: RootState) => state.user);
+
+  if (status.getMember === 'pending') {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="layout">
       <Header />
-      <Resume />
+      {member?.profile !== null ? <ResumeView /> : <Resume />}
+
       <Footer />
     </div>
   );
