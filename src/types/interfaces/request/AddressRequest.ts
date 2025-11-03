14a320a9f6916ddffb5415fc902c5d7cd155560c
Jupiter <kjimin2123@gmail.com>
2025-11-03 14:45:34 +0900

    refactor(types): Remove unused interface files
    - 사용되지 않는 인터페이스 파일들을 삭제하여 코드베이스를 정리했습니다.
    - 이 변경으로 코드의 가독성과 유지보수성이 향상됩니다.

--- src/types/interfaces/request/AddressRequest.ts
+++
@@ -1,7 +0,0 @@
-export interface AddressRequest {
-  address: string; // 주소
-  addressDetail: string; // 상세 주소
-  addressJibun: string; // 지번 주소
-  regionCity: string; // 시/군/구
-  zonecode: string; // 우편번호
-}
--- src/types/interfaces/request/LoginRequest.ts
+++
@@ -1,4 +0,0 @@
-export interface LoginRequestDTO {
-  email: string; // 사용자 이름
-  password: string; // 비밀번호
-}
