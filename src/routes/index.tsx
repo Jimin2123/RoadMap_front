9df386f22f0b9f4abe6f22244cbddb7c1ba76224
Jupiter <kjimin2123@gmail.com>
2025-12-01 16:36:22 +0900

    feat(routes): Add route for Interested Companies page
    - 관심 있는 회사 페이지에 대한 경로를 추가했습니다.
    - 이 페이지는 보호된 경로로 설정되어 있습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/LoginPage';
 import SettingsPage from '../pages/SettingsPage';
 import SignUpForm from '../components/SignUp/SignUpForm';
-import TariningCoursePage from '../pages/TrainingCoursePage';
 import ResumePage from '../pages/ResumePage';
 import PolicyPage from '../pages/PolicyPage';
 import DiagnosisPage from '../pages/DiagnosisPage';
 import MatchingPage from '../pages/MatchingPage';
 import ProtectedRoute from './ProtectedRoute';
+import InterestedCompaniesPage from '../pages/InterestedCompaniesPage';
 
 const AppRoutes = () => {
   return (
@@ -48,7 +48,14 @@ const AppRoutes = () => {
           </ProtectedRoute>
         }
       />
-      <Route path="/training" element={<TariningCoursePage />} /> {/* 직업 훈련 페이지 */}
+      <Route
+        path="/interested-companies"
+        element={
+          <ProtectedRoute>
+            <InterestedCompaniesPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/policy" element={<PolicyPage />} /> {/* 정책 페이지 */}
       <Route path="*" element={<MainPage />} /> {/* 잘못된 경로는 메인 페이지로 리다이렉트 */}
     </Routes>
